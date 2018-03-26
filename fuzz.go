dd3e6b59299349cf8712a6444cd93117cce61371
Christopher Schinnerl <chris@sia.tech>
2018-03-26 11:27:32 -0400

    implement review changes

--- fuzz.go
+++ fuzz.go
@@ -5,8 +5,6 @@ package merkletree
 import (
 	"bytes"
 	"crypto/sha256"
-	"encoding/binary"
-	"errors"
 	"io"
 )
 
@@ -52,11 +50,9 @@ func FuzzReadSubTreesWithProof(data []byte) int {
 	tree.SetIndex(index)
 	data = data[2:]
 
-	// 32 is the length of a sha256 hash and 4 bytes are used for the height of
-	// the subTree.
-	subTreeSize := 4 + 36
-	err := tree.readSubTrees(bytes.NewReader(data), subTreeSize)
-	if err != io.ErrUnexpectedEOF {
+	subTreeSize := 1 + 32 // 1 byte height + 32 bytes hash
+	err := tree.readSubTrees(bytes.NewReader(data))
+	if err != nil && err != io.ErrUnexpectedEOF {
 		return 0
 	} else if err == io.ErrUnexpectedEOF && len(data) < subTreeSize {
 		return -1
@@ -84,11 +80,9 @@ func FuzzReadSubTreesWithProof(data []byte) int {
 func FuzzReadSubTreesNoProof(data []byte) int {
 	tree := New(sha256.New())
 
-	// 32 is the length of a sha256 hash and 4 bytes are used for the height of
-	// the subTree.
-	subTreeSize := 4 + 36
-	err := tree.readSubTrees(bytes.NewReader(data), subTreeSize)
-	if err != io.ErrUnexpectedEOF {
+	subTreeSize := 1 + 32 // 1 byte height + 32 bytes hash
+	err := tree.readSubTrees(bytes.NewReader(data))
+	if err != nil && err != io.ErrUnexpectedEOF {
 		return 0
 	} else if err == io.ErrUnexpectedEOF && len(data) < subTreeSize {
 		return -1
@@ -96,8 +90,8 @@ func FuzzReadSubTreesNoProof(data []byte) int {
 	if tree.head != nil && tree.Root() == nil {
 		panic("root shouldn't be nil for a non-empty tree")
 	}
-	// The data is better if it contains many subTrees.
-	if len(data) > 100*subTreeSize {
+	// The data should at least contain 2 subtrees.
+	if len(data) > 2*subTreeSize {
 		return 1
 	}
 	return 0
@@ -105,11 +99,10 @@ func FuzzReadSubTreesNoProof(data []byte) int {
 
 // readSubTrees is a helper function that maps the data from a io.Reader to
 // subTrees and adds them to a tree.
-func (t *Tree) readSubTrees(r io.Reader, subTreeSize int) error {
-	if subTreeSize <= 4 {
-		return errors.New(`the subTree must at least contain 4 bytes for the height
-		plus some data for the sum`)
-	}
+func (t *Tree) readSubTrees(r io.Reader) error {
+	// 32 is the length of a sha256 hash and 1 byte is used for the height of
+	// the subTree.
+	subTreeSize := 1 + 32
 	for {
 		subTree := make([]byte, subTreeSize)
 		_, readErr := io.ReadFull(r, subTree)
@@ -119,9 +112,9 @@ func (t *Tree) readSubTrees(r io.Reader, subTreeSize int) error {
 		} else if readErr != nil {
 			return readErr
 		}
-		// The first 4 bytes of the subTree are mapped to a height in range
-		// [0,50].
-		height := int(binary.LittleEndian.Uint32(subTree[:4])) % 51
+		// The first byte of the subTree are mapped to a height in range
+		// [0,4].
+		height := int(subTree[0]) % 5
 		sum := subTree[4:]
 		if height > 0 {
 			if err := t.PushSubTree(height, sum); err != nil {
